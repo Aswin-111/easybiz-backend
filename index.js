eda1baa82757da8c48e72023572fbd421e3d80c3
Aswin <aswindkumar764@gmail.com>
2024-02-17 21:43:25 +0530

    added limit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const cust = require('./models/custmast')
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit : "10mb"}))
 app.post('/login',async (req,res)=>{
 
     const {username,password} = req.body
